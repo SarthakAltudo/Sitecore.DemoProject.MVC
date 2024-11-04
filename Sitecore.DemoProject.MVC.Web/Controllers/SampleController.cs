4a5b1ea3ffbdcc9a9f65dafae6c81de6b7cf9cda
Sarthak.Pandey <Sarthak.Pandey@ALT-LT-822>
2024-11-04 21:43:25 +0530

    Add project files.

---
+++ Sitecore.DemoProject.MVC.Web/Controllers/SampleController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Sitecore.DemoProject.MVC.Web.Controllers
+{
+    public class SampleController : Controller
+    {
+        public ActionResult List()
+        {
+            List<string> model = new List<string>();
+            model.Add("Sarthak");
+            model.Add("Sushant");
+            model.Add("Abhishek");
+            model.Add("Yash");
+            model.Add("Abhuday");
+            return View(model); //return model details to the view
+        }
+    }
+}
